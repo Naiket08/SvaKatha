56e7cb59a9fc6962b7f4b47582962334b5dd78dc
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2020-03-16 00:15:33 +0530

    Facebook Integrated but not working properly
    If anyone has solution than try to figure out it and solve it

--- app/src/main/java/com/example/svakatha/SignupScreen.java
+++ app/src/main/java/com/example/svakatha/SignupScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -13,6 +14,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
@@ -20,6 +30,11 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class SignupScreen extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
@@ -30,6 +45,8 @@ public class SignupScreen extends AppCompatActivity implements GoogleApiClient.O
     SignInButton signInButton;
     private GoogleApiClient googleApiClient;
     private static final int RC_SIGN_IN = 1;
+    CallbackManager callbackManager;
+    LoginButton facebook_login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +63,7 @@ public class SignupScreen extends AppCompatActivity implements GoogleApiClient.O
         editTextSignUpEmailId = (EditText)findViewById(R.id.editTextSignUpEmailId);
         editTextSignUpPassword = (EditText)findViewById(R.id.editTextSignUpPassword);
         editTextSignUpConfirmPassword = (EditText)findViewById(R.id.editTextSignUpConfirmPassword);
+        boolean loggedOut = AccessToken.getCurrentAccessToken() == null;
 
         buttonSignUp_LetsStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +120,66 @@ public class SignupScreen extends AppCompatActivity implements GoogleApiClient.O
                 startActivityForResult(intent,RC_SIGN_IN);
             }
         });
+
+        if (!loggedOut) {
+            Log.d("TAG", "Username is: " + Profile.getCurrentProfile().getName());
+
+            //Using Graph API
+            getUserProfile(AccessToken.getCurrentAccessToken());
+        }
+
+        callbackManager = CallbackManager.Factory.create();
+        facebook_login_button = (LoginButton) findViewById(R.id.facebook_login_button);
+        facebook_login_button.setReadPermissions(Arrays.asList("email","public_profile"));
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        facebook_login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                boolean loggedIn = AccessToken.getCurrentAccessToken() == null;
+                Log.d("API123", loggedIn + " ??");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    private void getUserProfile(AccessToken currentAccessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(
+                currentAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.d("TAG", object.toString());
+                        try {
+                            String first_name = object.getString("first_name");
+
+                            Intent intent = new Intent(SignupScreen.this,DetailsScreen.class);
+                            intent.putExtra("Name",first_name);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name");
+        request.setParameters(parameters);
+        request.executeAsync();
+
     }
 
     @Override
@@ -111,6 +189,7 @@ public class SignupScreen extends AppCompatActivity implements GoogleApiClient.O
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==RC_SIGN_IN){
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
