913e55bf03a759eb5af89d9e4dea9311b62ad432
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2020-03-16 18:01:57 +0530

    final page ho gaya

---
+++ app/src/main/java/com/example/svakatha/ImageSelection.java
@@ -0,0 +1,14 @@
+package com.example.svakatha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_selection);
+    }
+}
