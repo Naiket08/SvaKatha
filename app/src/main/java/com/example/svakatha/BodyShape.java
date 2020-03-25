a9bd3de96dd67d1dd462bd5671ee3ab460b7bac8
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2020-03-25 13:53:27 +0530

    Bodyshape

--- app/src/main/java/com/example/svakatha/BodyShape.java
+++ app/src/main/java/com/example/svakatha/BodyShape.java
@@ -6,7 +6,9 @@ import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
@@ -25,23 +27,33 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
+import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BodyShape extends AppCompatActivity {
 
-    private ImageView imageViewBodyShapeScreen;
+    private ImageView imageViewBodyShapeScreen,imageview1,imageview2;
     private TextView textViewBodyShapeText2, textViewBodyShapeText3, textViewBodyShapeText4;
     private ProgressBar progressBarBodyShapeScreen;
     private ImageButton imageButtonBody1, imageButtonBody2, imageButtonBody3, imageButtonBody4, imageButtonBody5, imageButtonBodyShapeScreenForward;
     private FirebaseAuth auth;
+    private StorageReference mStorage;
+    List<Uri> downloadLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_shape);
         //casting of ImaegView
+        downloadLink=new ArrayList<>();
         imageViewBodyShapeScreen = (ImageView) findViewById(R.id.imageViewBodyShapeScreen_1);
         //casting of textView
         textViewBodyShapeText2 = (TextView) findViewById(R.id.textViewBodyShapeText2_1);
@@ -63,6 +75,9 @@ public class BodyShape extends AppCompatActivity {
         imageButtonBody3 = (ImageButton) findViewById(R.id.imagebuttonbody3);
         imageButtonBody4 = (ImageButton) findViewById(R.id.imagebuttonbody4);
         imageButtonBody5 = (ImageButton) findViewById(R.id.imagebuttonbody5);
+        //imageview1=findViewById(R.id.imageviewbody1);
+        //imageview2=findViewById(R.id.imageviewbody2);
+
 
         final String currentID = auth.getCurrentUser().getUid();
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -71,6 +86,60 @@ public class BodyShape extends AppCompatActivity {
         textViewBodyShapeText2.setTypeface(textViewBodyShapeText2.getTypeface(), Typeface.BOLD);
         String name_bodyshape = intent.getStringExtra("Name_skintone");
         textViewBodyShapeText2.setText("Hi"+" "+name_bodyshape);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mStorage.child("BodyShapeMale").listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        for(StorageReference ref :listResult.getItems()) {
+                            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    downloadLink.add(uri);
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+            /*imageview1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imageview1.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                    imageButtonBody5.getBackground().clearColorFilter();
+                    imageButtonBody3.getBackground().clearColorFilter();
+                    imageButtonBody4.getBackground().clearColorFilter();
+                    imageview2.getBackground().clearColorFilter();
+
+                    String bodyshape="Shape1";
+                    Picasso.get().load(downloadLink.get(0)).into(imageview1);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("BodyShape", bodyshape);
+                    db.collection("users").document(currentID).set(user, SetOptions.merge());
+
+                }
+            });*/
+
+        /*imageview2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageview2.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                imageButtonBody5.getBackground().clearColorFilter();
+                imageButtonBody3.getBackground().clearColorFilter();
+                imageButtonBody4.getBackground().clearColorFilter();
+                imageview1.getBackground().clearColorFilter();
+
+                Picasso.get().load(downloadLink.get(1)).into(imageview2);
+
+                String bodyshape="Shape2";
+                Map<String, Object> user = new HashMap<>();
+                user.put("BodyShape", bodyshape);
+                db.collection("users").document(currentID).set(user, SetOptions.merge());
+
+            }
+        });*/
 
 
         imageButtonBody1.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +151,7 @@ public class BodyShape extends AppCompatActivity {
                 imageButtonBody4.getBackground().clearColorFilter();
                 imageButtonBody2.getBackground().clearColorFilter();
                 String bodyshape="Shape1";
+                Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/svakatha-af104.appspot.com/o/BodyShapeMale%2FMaleAthletic.jpg?alt=media&token=6dcf6d96-b755-4734-ada3-3245fdaefbde").into(imageButtonBody1);
                 Map<String, Object> user = new HashMap<>();
                 user.put("BodyShape", bodyshape);
                 db.collection("users").document(currentID).set(user, SetOptions.merge());
