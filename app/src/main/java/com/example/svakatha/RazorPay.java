da22b374ff8c563ec5c0b3ea1e6d7a07e024cb1e
Snehal Singh <53808476+Snehal-Singh174@users.noreply.github.com>
2020-03-29 20:54:53 +0530

    Little changes

--- app/src/main/java/com/example/svakatha/RazorPay.java
+++ app/src/main/java/com/example/svakatha/RazorPay.java
@@ -45,7 +45,7 @@ public class RazorPay extends Activity implements PaymentResultListener {
         /**
          * Reference to current activity
          */
-        final RazorPay activity = this;
+        final Activity activity = this;
 
 
         /**
@@ -89,18 +89,18 @@ public class RazorPay extends Activity implements PaymentResultListener {
     @Override
     public void onPaymentSuccess(String s) {
         Toast.makeText(this, "Payment Successful", Toast.LENGTH_SHORT).show();
-        onBackPressed();
+        //onBackPressed();
     }
 
     @Override
     public void onPaymentError(int i, String s) {
-        onBackPressed();
+        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
     }
-    public void onBackPressed() {
+    /*public void onBackPressed() {
         if (getFragmentManager().getBackStackEntryCount() == 0) {
             this.finish();
         } else {
             getFragmentManager().popBackStack();
         }
-    }
+    }*/
 }
