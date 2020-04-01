12e7bae203d41bcb1e486366072ede739a0b940f
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2020-04-01 13:21:36 +0530

    Revert "optimized"
    This reverts commit 575b6a196999b4cd68a4dab51975111da7a66e90.

--- app/src/main/java/com/example/svakatha/Business.java
+++ app/src/main/java/com/example/svakatha/Business.java
@@ -94,6 +94,12 @@ public class Business extends AppCompatActivity {
                 Bussiness = spnr_business.getSelectedItem().toString();
                 user.put("Business",Bussiness);
                 db.collection("users").document(userId).set(user, SetOptions.merge());
+                String X=String.valueOf(0);
+                user.put("AddCart",X);
+                db.collection("users").document(userId).set(user, SetOptions.merge());
+                String Y=String.valueOf(0);
+                user.put("Counter",Y);
+                db.collection("users").document(userId).set(user, SetOptions.merge());
 
             }
 
