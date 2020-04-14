6632c724cec5773a3f7ad0ec9038fb3443ad64ff
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2020-04-14 22:40:32 +0530

    Update Business.java

--- app/src/main/java/com/example/svakatha/Business.java
+++ app/src/main/java/com/example/svakatha/Business.java
@@ -72,7 +72,9 @@ public class Business extends AppCompatActivity {
         //casting of ImageView
         imageViewBusinessScreenHeader=(ImageView)findViewById(R.id.imageViewBusinessScreenHeader1);
 
-        String[] business = new String[]{"Casual","Ethnic","Wedding","Formal"};
+        String[] business = new String[]{"Ocassion","Business","Dinner party","A business dinner or a company party",
+                "Family Get-Together or Birthday Party","College","Cocktail party","Business Formal","Religious Ceremony",
+                "Interview","A Night at the Theater","Office","Other"};
         Spinner spnr_business = findViewById(R.id.spnr_occasion_bussiness);
         ArrayAdapter<String> businessarray =
                 new ArrayAdapter<>(
