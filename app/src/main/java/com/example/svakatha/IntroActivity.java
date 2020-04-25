c591816760016bd0be5f5dce2f3c0e22f068a9e1
NadeemAhemad <58600499+NadeemAhemad@users.noreply.github.com>
2020-04-25 17:37:50 +0530

    main changes

---
+++ app/src/main/java/com/example/svakatha/IntroActivity.java
@@ -0,0 +1,175 @@
+package com.example.svakatha;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter ;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0 ;
+    Animation btnAnim ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // make the activity on full screen
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        // when this activity is about to be launch we need to check if its openened before or not
+
+
+
+        setContentView(R.layout.activity_intro);
+
+        // hide the action bar
+
+        getSupportActionBar().hide();
+
+        // ini views
+        btnNext = findViewById(R.id.btn_next);
+
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+
+
+        // fill list screen
+
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("SVA-KATHA","Embracing The Differences",R.drawable.imga1));
+        mList.add(new ScreenItem("SVA-KATHA","Styles Handpicked Just For You",R.drawable.imga2));
+        mList.add(new ScreenItem("SVA-KATHA","We use Machine Learning and AI to give you suggestion like a personal Stylist and Designer",R.drawable.imga3));
+
+        // setup viewpager
+        screenPager =findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        // setup tablayout with viewpager
+
+        tabIndicator.setupWithViewPager(screenPager);
+
+        // next button click Listner
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+
+                    position++;
+                    screenPager.setCurrentItem(position);
+
+
+                }
+
+                if (position == mList.size()-1) { // when we rech to the last screen
+
+                    // TODO : show the GETSTARTED Button and hide the indicator and the next button
+
+                    loaddLastScreen();
+
+
+                }
+
+
+
+            }
+        });
+
+        // tablayout add change listener
+
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size()-1) {
+
+                    loaddLastScreen();
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            process1();
+                            //overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        }
+                    },1500);
+
+                }
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+        // Get Started button click listener
+    }
+    private void savePrefsData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpnend",true);
+        editor.commit();
+
+
+    }
+
+    // show the GETSTARTED Button and hide the indicator and the next button
+    private void loaddLastScreen() {
+
+        btnNext.setVisibility(View.INVISIBLE);
+        tabIndicator.setVisibility(View.VISIBLE);
+        // TODO : ADD an animation the getstarted button
+        // setup animation
+    }
+    public void process1(){
+        Intent mainActivity = new Intent(getApplicationContext(),SignupScreen.class);
+        startActivity(mainActivity);
+        // also we need to save a boolean value to storage so next time when the user run the app
+        // we could know that he is already checked the intro screen activity
+        // i'm going to use shared preferences to that process
+        savePrefsData();
+        finish();
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/svakatha/IntroViewPagerAdapter.java
@@ -0,0 +1,83 @@
+package com.example.svakatha;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class IntroViewPagerAdapter extends PagerAdapter {
+
+    Context mContext ;
+    List<ScreenItem> mListScreen;
+
+    public IntroViewPagerAdapter(Context mContext, List<ScreenItem> mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.layout_screen,null);
+
+        ImageView imgSlide = layoutScreen.findViewById(R.id.intro_img);
+        TextView title = layoutScreen.findViewById(R.id.intro_title);
+        TextView description = layoutScreen.findViewById(R.id.intro_description);
+
+        title.setText(mListScreen.get(position).getTitle());
+        description.setText(mListScreen.get(position).getDescription());
+        if(position==0)
+        {
+            title.setTextColor(Color.parseColor("#693E32"));
+        }
+        if(position==1)
+        {
+            title.setTextColor(Color.parseColor("#B59C85"));
+        }
+        else
+        {
+            title.setTextColor(Color.parseColor("#B59C85"));
+
+        }
+
+
+        imgSlide.setImageResource(mListScreen.get(position).getScreenImg());
+
+        container.addView(layoutScreen);
+
+        return layoutScreen;
+
+
+
+
+
+    }
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+
+        container.removeView((View)object);
+
+    }
+}
